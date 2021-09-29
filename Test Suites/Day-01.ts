88cd3458683319528cc9ed220c00040aa5af5070
ntbichthuy07@gmail.com <jade3O07>
2021-09-29 23:16:23 +0700

    Day-01_Recording guru99

---
+++ Test Suites/Day-01.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Day-01</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>e6213504-f67f-446b-98ce-f1bf5690b795</testSuiteGuid>
+   <testCaseLink>
+      <guid>f6a70390-5e9a-4433-8835-d8581fecb175</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Day-01_Recording Guru99</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>

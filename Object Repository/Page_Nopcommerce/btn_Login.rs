45459ee009f64e174b41dbccdea035d26d7f0ef6
ntbichthuy07@gmail.com <jade3O07>
2021-09-30 23:10:47 +0700

    day-02 Object Repository

---
+++ Object Repository/Page_Nopcommerce/btn_Login.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>btn_Login</name>
+   <tag></tag>
+   <elementGuidId>9cb82648-230e-4b8d-a87a-d8911d5202b9</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//input[@type=&quot;submit&quot;]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
